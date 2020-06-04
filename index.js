f08baf0e959d3f882da9a6bec739e8d52564ecda
stefian <stefan.ianta@w3ai.net>
2020-06-04 20:55:44 -0300

    added app.get /posts and app.post /events endpoints

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get('/posts', (req, res) => {
+
+});
+
+// the endpoint that will receive events froom the event bus
+app.post('/events', (req, res) => {
+
+});
+
+app.listen(4002, () => {
+  console.log('Query: Listening on 4002')
+});
