1147078f1f707688fb8823551b18de96ddb0f31f
stefian <stefan.ianta@w3ai.net>
2020-06-06 15:32:52 -0300

    added require axios

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
