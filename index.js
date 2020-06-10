00c70a7c94e6f5bbe02d26279680ad07ad037937
stefian <stefan.ianta@w3ai.net>
2020-06-10 16:13:45 -0300

    updated localhost to clusterip route event-bus-srv

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.post('/events', (req, res) => {
 app.listen(4002, async () => {
   console.log('Query: Listening on 4002');
 
-  const res = await axios.get('http://localhost:4005/events');
+  const res = await axios.get('http://event-bus-srv:4005/events');
   // with axios the data is available in res.data
   for (let event of res.data) {
     console.log('Processing event: ', event.type);
